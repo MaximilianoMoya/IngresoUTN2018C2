a5b34241af40325daf78e25c53a264697c30f3fd
MaximilianoMoya <40808606+MaximilianoMoya@users.noreply.github.com>
2018-07-04 15:34:43 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var num1;
+    var num2;
+    var total;
+
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    total = num1 + num2;
+
+    alert("la suma es: " + total);
 }
 
 function restar()
 {
-	
+    var num1;
+    var num2;
+    var total;
+
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    total = num1 - num2;
+
+    alert("la resta es: " + total);
 }
 
 function multiplicar()
 { 
-	
+    var num1;
+    var num2;
+    var total;
+
+    num1=document.getElementById("numeroUno").value;
+    num2=document.getElementById("numeroDos").value;
+
+    num1=parseInt(num1);
+    num2=parseInt(num2);
+
+    total = num1 * num2;
+
+    alert("la multiplicación es: " + total);
 }
 
 function dividir()
 {
-	
+    var num1;
+    var num2;
+    var total;
+
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    total = num1 / num2;
+
+    alert("la división es: " + total);
 }
 
