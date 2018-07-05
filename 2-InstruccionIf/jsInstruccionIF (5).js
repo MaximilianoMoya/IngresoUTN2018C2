5b5bf7d7c3050fe5466d61ba105829a17d4142bf
MaximilianoMoya <40808606+MaximilianoMoya@users.noreply.github.com>
2018-07-04 22:26:51 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+if (edad >= 13 && edad <= 17)
+{
+    
+}
+
+else 
+{
+    alert ("No es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+
+if (edad >= 13 && edad <= 17)
+{
+    alert("Es adolescente");
+}
+
+else 
+{
+    alert ("No es adolescente");
+}
 
 
 
