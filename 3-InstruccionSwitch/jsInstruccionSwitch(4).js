dbf6532cedbaf05db8083928b678ccdfe25a9e6d
MaximilianoMoya <40808606+MaximilianoMoya@users.noreply.github.com>
2018-07-11 22:14:18 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,7 +3,25 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch (mesDelAño) {
+
+    case "Febrero":
+    alert("tiene 28 dìas");
+    break;
+    
+    case "Enero":
+    case "Marzo":
+    case "Mayo" :
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+    alert("tiene 31 dìas");
+    break;
+    
+    default : 
+    alert("tiene 30 dìas");
+}
 	
 	
 
