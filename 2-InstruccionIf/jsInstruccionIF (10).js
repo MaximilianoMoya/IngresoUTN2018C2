c46b30493d2443266b7d903c70365c07c6c7c2ee
MaximilianoMoya <40808606+MaximilianoMoya@users.noreply.github.com>
2018-07-11 21:07:12 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,5 +1,22 @@
 function mostrar()
 {
+	var nota;
+
+	nota = Math.floor(Math.random() * 10 + 1);
+
+	if (nota >= 9){
+		alert("Excelente");
+}
+
+	else if (nota >=4 && nota <9) {
+	
+	alert("Aprobò");
+
+}
+	else  {
+	alert("Vamos, la proxima se puede");
+}
+
 	//Genero el número RANDOM entre 1 y 10 
 	
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+var num1;
+var num2;
+var suma;
 
+num1 = prompt("Coloque el primer numero");
+num2 = prompt("Coloque el segundo numero");
+
+if (num1 == num2){
+    alert(num1 + num2);
 }
+else if (parseInt(num1) > parseInt(num2)) {
+    alert(num1-num2);
+}
+
+else { 
+    
+    suma = parseInt(num1) + parseInt(num2);
+    
+    if (suma > 10) {
+        alert("La suma es: " + suma + " y superò el 10");
+    }
+    
+    else {
+    alert("La suma es: " + suma);
+    }
+}
+}
+
+
