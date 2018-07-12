84b10dfb58950c88fee325fb2568ff35ab38ed78
MaximilianoMoya <40808606+MaximilianoMoya@users.noreply.github.com>
2018-07-11 21:54:31 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,23 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño) {
+
+    case "Enero" : 
+        alert("Que empieces bien el año!");
+        break;
+    case "Marzo" :
+        alert("A clases!");
+        break;
+    case "Julio" : 
+        alert("Felices vacaciones de invierno!");
+        break;
+    case "Diciembre" :
+        alert("Felices fiestas!");
+        break;
+
+}
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,31 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño) {
+
+    case "Marzo" :
+    case "Abril" :
+    case "Mayo" :
+    case "Junio" :
+    alert("Falta para el invierno");
+    break;
+    case "Julio" :
+    case "Agosto" :
+    alert("Abrigate que hace frio");
+    break;
+    case "Septiembre" :
+    case "Octubre" :
+    case "Noviembre" :
+    case "Diciembre" :
+    case "Enero" : 
+    case "Febrero" :
+    alert("Ya pasò el frio, ahora calor");
+    break;
+    
+    
 
 
 
+}
 
 }//FIN DE LA FUNCIÓN
